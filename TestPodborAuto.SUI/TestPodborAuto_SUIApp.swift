32c08021e79e3277a2d47529becebfbec8a870a0
Igor Rukaleev <575714470@mail.ru>
2023-08-04 14:36:30 +0300

    Initial Commit

---
+++ TestPodborAuto.SUI/TestPodborAuto_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPodborAuto_SUIApp.swift
+//  TestPodborAuto.SUI
+//
+//  Created by Igor on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestPodborAuto_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
