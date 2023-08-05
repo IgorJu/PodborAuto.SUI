7237c78c8f8be5ad81f32873da96861ee05497dc
Igor Rukaleev <575714470@mail.ru>
2023-08-05 22:17:28 +0300

    update sort by price code

--- TestPodborAuto.SUI/Views/MainView.swift
+++ TestPodborAuto.SUI/Views/MainView.swift
@@ -12,6 +12,7 @@ struct MainView: View {
     @EnvironmentObject var data: DataStore
     @State private var isShowingAddCarScreen = false
     @State private var car: Car? = nil
+    @State private var sortByHighestPrice: Bool = false
     
     var body: some View {
         ZStack {
@@ -26,17 +27,20 @@ struct MainView: View {
                     }
                     .padding(.leading, 10)
                     Spacer()
-                    Text("Сортировка по цене")
-                        .foregroundColor(.blue)
-                    Button(action: sortByLowestPrice) {
-                        Image(systemName: "arrowtriangle.up")
+                    Button(action: {
+                        sortByPrice()
+                        sortByHighestPrice.toggle()
+                    }) {
+                        Text("Сортировка по цене")
+                            .foregroundColor(.blue)
+                        if !sortByHighestPrice {
+                            Image(systemName: "arrow.up")
+                                .padding(.trailing, 10)
+                        } else {
+                            Image(systemName: "arrow.down")
+                                .padding(.trailing, 10)
+                        }
                     }
-                    .font(.title2)
-                    Button(action: sortByHighestPrice) {
-                        Image(systemName: "arrowtriangle.down")
-                    }
-                    .font(.title2)
-                    .padding(.trailing, 10)
                 }
                 .sheet(isPresented: $isShowingAddCarScreen) {
                     AddCarView(car: $car) { newCar in
@@ -56,29 +60,25 @@ struct MainView: View {
         }
     }
     
-    // Добавьте функцию для создания Binding для car
+    //функция для создания Binding для car
     private func binding(for car: Car) -> Binding<Car?> {
-           guard let carIndex = data.cars.firstIndex(where: { $0.id == car.id }) else {
-               fatalError("Car not found")
-           }
-           return Binding<Car?>(
-               get: { data.cars[carIndex] },
-               set: { data.cars[carIndex] = $0 ?? Car(id: UUID(), brand: "", model: "", mileage: 0, countOfOwners: 0, yearOfRelease: "", price: 0, imageName: "") }
-           )
-       }
-   
-    private func sortByLowestPrice() {
-        data.cars.sort { $0.price < $1.price }
+        guard let carIndex = data.cars.firstIndex(where: { $0.id == car.id }) else {
+            fatalError("Car not found")
+        }
+        return Binding<Car?>(
+            get: { data.cars[carIndex] },
+            set: { data.cars[carIndex] = $0 ?? Car(id: UUID(), brand: "", model: "", mileage: 0, countOfOwners: 0, yearOfRelease: "", price: 0, imageName: "") }
+        )
     }
     
-    private func sortByHighestPrice() {
-        data.cars.sort { $0.price > $1.price }
+    private func sortByPrice() {
+        if sortByHighestPrice {
+            data.cars.sort { $0.price < $1.price }
+        } else {
+            data.cars.sort { $0.price > $1.price }
+        }
     }
 }
-    
-    
-
-
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
